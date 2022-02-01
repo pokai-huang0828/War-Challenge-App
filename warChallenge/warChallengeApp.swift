1bd0c46753ddcdb7ea2bf8df0f43cdc1f9b3e540
Po-Kai Huang <71723684+pokai-huang0828@users.noreply.github.com>
2022-01-31 21:36:58 -0800

    Initial Commit

---
+++ warChallenge/warChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warChallengeApp.swift
+//  warChallenge
+//
+//  Created by PoKai Huang on 2022-01-31.
+//
+
+import SwiftUI
+
+@main
+struct warChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
