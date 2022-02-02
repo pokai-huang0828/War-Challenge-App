62d1d13b0f5b3a1d17060c57e3c34f3847597044
Po-Kai Huang <71723684+pokai-huang0828@users.noreply.github.com>
2022-02-01 22:38:49 -0800

    Added card function and make the app working

--- warChallenge/ContentView.swift
+++ warChallenge/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         
         ZStack{
@@ -21,15 +27,35 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
                 
                 Button(action: {
                     print("Button clicked!")
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update the score
+                    if (playerRand > cpuRand){
+                        playerScore += 1
+                    }
+                    else if (playerRand < cpuRand) {
+                        cpuScore += 1
+                    }
+                    else{
+                        playerScore += 1
+                        cpuScore += 1
+                    }
                 }, label: {
                     Image("dealbutton")
                     
@@ -44,7 +70,7 @@ struct ContentView: View {
                             .font(.title)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 5.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
@@ -54,7 +80,7 @@ struct ContentView: View {
                             .font(.title)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 5.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
